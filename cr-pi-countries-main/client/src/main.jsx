100878db5d7c89a3c2b5166b2a61117d5afbcca9
Nicolas Soto <nicosoto18@hotmail.com>
2024-03-18 11:34:09 -0300

    probando error de direccionamiento

--- cr-pi-countries-main/client/src/main.jsx
+++ cr-pi-countries-main/client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import Store from './Components/redux/store.js'
+import Store from './Components/redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
