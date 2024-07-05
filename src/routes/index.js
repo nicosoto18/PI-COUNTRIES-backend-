4191e61ef80e573e8229ec5a61796041ff8e512b
Nicolas Soto <nicosoto18@hotmail.com>
2024-07-05 16:13:03 -0300

    Cambiando de hosting la DB

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,10 @@ const getCountriesByName = require("../../controllers/getCountriesByName");
 const createActivities = require("../../controllers/createActivities");
 const getActivities=require("../../controllers/getActivities");
 
-
 router.get("/countries",getCountries);
 router.get("/countries/:Id",getCountryId);
 router.get("/countriesByName", getCountriesByName);
 router.get("/activities", getActivities);
 router.post("/activities", createActivities);
-
 
 module.exports = router;
